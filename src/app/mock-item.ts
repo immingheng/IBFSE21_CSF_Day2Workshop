99f436c8ef87cc1585ead597ffbf3442831de751
immingheng <im.mingheng@gmail.com>
2022-01-25 16:35:10 +0800

    Final commit

--- src/app/mock-item.ts
+++ src/app/mock-item.ts
@@ -1,10 +1,10 @@
 import {Item} from './item';
 
 export const ITEMS: Item[] = [
-  {thumbnail:"../assets/placeholder.png", name: 'Item 1', quantity: 0},
-  {thumbnail:"../assets/placeholder.png", name: 'Item 2', quantity: 0},
-  {thumbnail:"../assets/placeholder.png", name: 'Item 3', quantity: 0},
-  {thumbnail:"../assets/placeholder.png", name: 'Item 4', quantity: 0}
+  {thumbnail:'../assets/placeholder.png', name: 'Item 1', quantity: 0},
+  {thumbnail:'../assets/placeholder.png', name: 'Item 2', quantity: 0},
+  {thumbnail:'../assets/placeholder.png', name: 'Item 3', quantity: 0},
+  {thumbnail:'../assets/placeholder.png', name: 'Item 4', quantity: 0}
 
 
 ]
